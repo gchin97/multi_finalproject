cf5427d5bc14f0a3e17c2742619ea88220084653
JeUkYun <yju1205@gmail.com>
2023-10-20 13:02:29 +0900

    제욱현아민주 합본

--- Web_service/prediction/views.py
+++ Web_service/prediction/views.py
@@ -30,58 +30,78 @@ def emp(request):
     city_list = EmpInfo.objects.values_list('city', flat=True).distinct()
     job_list = EmpInfo.objects.values_list('job_name', flat=True).distinct()
 
-    if request.method == 'POST':
-        # 필터링을 적용한 경우
-        selected_cities = request.POST.getlist("city")
-        selected_jobs = request.POST.getlist("job_name")
+    # 초기 페이지 로드
+    emp_list = EmpInfo.objects.all().values('city', 'job_name', 'company', 'link', 'ncs_code').distinct()
+    education_list = Education.objects.all().values('train_title', 'start_date', 'end_date', 'train_center', 'quota', 'link').distinct()
+    center_list = EducationCenter.objects.filter(train_center__in=education_list.values_list('train_center', flat=True)).values('address', 'center_tel').distinct()
 
-        emp_list = EmpInfo.objects.all().values('city', 'job_name', 'company', 'link', 'ncs_code').distinct()
+# 고유한 'city', 'company', 'link', 'job_name' 정보를 포함하는 사전의 목록
+    unique_emp_list = []
 
-        if selected_cities:
-            emp_list = emp_list.filter(city__in=selected_cities).values('city', 'job_name', 'company', 'link', 'ncs_code').distinct()
-        if selected_jobs:
-            emp_list = emp_list.filter(job_name__in=selected_jobs).values('city', 'job_name', 'company', 'link', 'ncs_code').distinct()
+    for emp in emp_list:
+        city = emp['city']
+        job_name = emp['job_name']
+        company = emp['company']
+        link = emp['link']
 
-        paginator = Paginator(emp_list, 10)
-        page = request.GET.get('page', '1')
-        page_obj = paginator.get_page(page)
-    else:
-        # 초기 페이지 로드
-        emp_list = EmpInfo.objects.all().values('city', 'job_name', 'company', 'link', 'ncs_code').distinct()
-        paginator = Paginator(emp_list, 10)
-        page = request.GET.get('page', '1')
-        page_obj = paginator.get_page(page)
+        # 이미 목록에 있는 회사와 직업인지 확인하고 중복을 방지합니다.
+        if not any(entry['company'] == company and entry['job_name'] == job_name  for entry in unique_emp_list):
+            unique_emp_list.append({'city': city, 'job_name': job_name, 'company': company, 'link': link})
 
-    context = {'emp_list': page_obj, 'city_list': city_list, 'job_list': job_list}
+
+    paginator = Paginator(unique_emp_list, 5)
+    page = request.GET.get('page', '1')
+    page_obj = paginator.get_page(page)
+
+    context = {'emp_list': page_obj, 'city_list': city_list, 'job_list': job_list, 'education_list' : education_list, 'center_list': center_list}
     return render(request, 'prediction/emp.html', context)
-
+    
 def search(request):
     city_list = EmpInfo.objects.values_list('city', flat=True).distinct()
     job_list = EmpInfo.objects.values_list('job_name', flat=True).distinct()
+
+    error_message = None
 
     if request.user.is_authenticated:
         if request.method == 'GET':
             selected_cities = request.GET.getlist("city")
             selected_jobs = request.GET.getlist("job_name")
 
+            # selected_jobs에 있는 각 직업의 ncs_code를 가져옴
+            ncs_codes = EmpInfo.objects.filter(job_name__in=selected_jobs).values_list('ncs_code', flat=True).distinct()
             emp_list = EmpInfo.objects.all()
-
+            education_list = Education.objects.all()
+                
             if selected_cities:
                 emp_list = emp_list.filter(city__in=selected_cities).values('city', 'job_name', 'company', 'link', 'ncs_code').distinct()
+
             if selected_jobs:
                 emp_list = emp_list.filter(job_name__in=selected_jobs).values('city', 'job_name', 'company', 'link', 'ncs_code').distinct()
+                education_list = education_list.filter(ncs_code__in=ncs_codes).distinct()
 
-            return render(request, 'prediction/emp.html', {'city_list': city_list, 'job_list': job_list, 'emp_list': emp_list})
+            if not selected_cities and not selected_jobs:
+                    error_message = "적어도 하나의 지역 또는 직업을 선택해주세요."
+                    emp_list = []  
+                    education_list = []              
+
+            # 고유한 'city', 'company', 'link', 'job_name' 정보를 포함하는 사전의 목록
+            unique_emp_list = []
+
+            for emp in emp_list:
+                city = emp['city']
+                job_name = emp['job_name']
+                company = emp['company']
+                link = emp['link']
+
+                # 이미 목록에 있는 회사와 직업인지 확인하고 중복을 방지합니다.
+                if not any(entry['company'] == company and entry['job_name'] == job_name  for entry in unique_emp_list):
+                    unique_emp_list.append({'city': city, 'job_name': job_name, 'company': company, 'link': link})
+
+            paginator = Paginator(unique_emp_list, 5)
+            page = request.GET.get('page', '1')
+            page_obj = paginator.get_page(page)
+
+            return render(request, 'prediction/emp.html', {'city_list': city_list, 'job_list': job_list, 'education_list' : education_list, 'emp_list': page_obj , 'error_message':error_message})
     else:
         return render(request, 'common/login.html')
     
-def education(request, ncs_code):
-    if request.method == 'GET':
-        # Education 모델에서 ncs_code로 조회
-        education_info = Education.objects.filter(ncs_code=ncs_code).values('train_title', 'start_date', 'end_date', 'train_center', 'quota', 'link')
-
-        # EducationCenter 모델에서 train_center로 조회
-        center_info = EducationCenter.objects.filter(train_center__in=education_info.values_list('train_center', flat=True)).values('address', 'center_tel')
-        
-        context = {'education_info': education_info, 'center_info': center_info}
-        return render(request, 'prediction/education.html', context)
